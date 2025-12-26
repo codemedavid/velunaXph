5750a4a5665b7d3488fbe902d8bcce425d854bac
Yna <yna@gmail.com>
2025-12-26 19:06:28 +0800

    fghj

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -343,7 +343,7 @@ const AdminDashboard: React.FC = () => {
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
-    if (password === 'Peptidepulse@Admin123' || password === 'VelunaXph@Admin123') {
+    if (password === 'Veluna@Admin!2025') {
       setIsAuthenticated(true);
       localStorage.setItem('peptide_admin_auth', 'true');
       setLoginError('');
